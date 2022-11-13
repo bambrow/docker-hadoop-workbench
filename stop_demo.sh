36777cfaea3a5a748dee5b0578cad9e075a0f8cf
geekyouth <forsupergeeker@gmail.com>
2022-11-13 15:10:40 +0800

    docker compose 不兼容

--- stop_demo.sh
+++ stop_demo.sh
@@ -3,5 +3,5 @@
 DOCKER_COMPOSE_FILE=docker-compose-spark3.0.2.yml
 
 SCRIPT_PATH=$(cd `dirname $0`; pwd)
-docker-compose -f ${SCRIPT_PATH}/${DOCKER_COMPOSE_FILE} down
+docker compose -f ${SCRIPT_PATH}/${DOCKER_COMPOSE_FILE} down
 
--- stop_demo_v2.sh
+++ stop_demo_v2.sh
@@ -3,4 +3,4 @@
 DOCKER_COMPOSE_FILE=docker-compose-v2.yml
 
 SCRIPT_PATH=$(cd `dirname $0`; pwd)
-docker-compose -f ${SCRIPT_PATH}/${DOCKER_COMPOSE_FILE} down
+docker compose -f ${SCRIPT_PATH}/${DOCKER_COMPOSE_FILE} down
